a61ab3b4731b4740477074269fcb649156aa6d19
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-09-22 12:24:27 +0200

    Implemento el poder añadir números y actualizo la funcion de caracter aleatorio

--- GeneradorContraseñas.py
+++ GeneradorContraseñas.py
@@ -3,29 +3,60 @@ import random
 MAYUSCULAS = "ABCDEFJHIJKLMNOPQRSTUVWKYZ"
 MINUSCULAS = "abcdefjhijlkmnoprqstuvwkyz"
 ESPECIALES = "$%&€@#+Ç"
+NUMEROS = "1234567890"
 
 password = ""
 
 # 1. Decidir los requisitos de la contraseña
 
 # longitud = int(input("¿De que tamaño deseas la contraseña?"))
-# espec_char = input("¿Deseas que tu contraseña incluya caracteres especiales?")
+decidir_especial = input("¿Deseas que tu contraseña incluya caracteres especiales?")
+decidir_numero = input("¿Deseas que tu contraseña incluya números?")
 
 # 2. Generar la contraseña
 
-def caracter_aleatorio(): 
+def caracter_aleatorio(especial, numero): 
     caracter = ""
-    
-    aleatorio = random.randint(1,9)
-    if aleatorio == 1 or aleatorio == 4 or aleatorio == 7:
-        caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))] 
-    elif aleatorio == 2 or aleatorio == 5 or aleatorio == 8:
-        caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
-    else: 
-        caracter = ESPECIALES[random.randint(0, (len(ESPECIALES)-1))]
+    if especial.lower() == "si":
+        if numero == "si":
+            aleatorio = random.randint(1,8)
+            if aleatorio == 1 or aleatorio == 8:
+                caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))] 
+            elif aleatorio == 2 or aleatorio == 7:
+                caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
+            elif aleatorio == 3 or aleatorio == 6: 
+                caracter = ESPECIALES[random.randint(0, (len(ESPECIALES)-1))]
+            else: 
+                caracter = NUMEROS[random.randint(0, len(NUMEROS))]
+        else: 
+            aleatorio = random.randint(1,6)
+            if aleatorio == 1 or aleatorio == 6:
+                caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))] 
+            elif aleatorio == 2 or aleatorio == 5:
+                caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
+            else:
+                caracter = ESPECIALES[random.randint(0, (len(ESPECIALES)-1))]
+    if especial.lower() == "no": 
+        if numero == "si":
+            aleatorio = random.randint(1,6)
+            if aleatorio == 1 or aleatorio == 6:
+                caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))] 
+            elif aleatorio == 2 or aleatorio == 5:
+                caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
+            else:
+                caracter = NUMEROS[random.randint(0, (len(NUMEROS)-1))]
+        if numero == "no":
+            aleatorio = random.randint(1,4)
+            if aleatorio == 1 or aleatorio == 4: 
+                caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))]
+            else: 
+                caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
+                
+
+
     return caracter
 
-print(caracter_aleatorio())
+print(caracter_aleatorio(decidir_especial, decidir_numero))
     
 # def generar_contraseña(longitud, caracter): 
 #     if caracter.lower() == "si": 

b255dd72a4a12cf74cb6b95875d2d7b6369d2e8b
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-09-22 13:03:51 +0200

    Añado fichero README.md y cambio un poco de sintaxis

--- GeneradorContraseñas.py
+++ GeneradorContraseñas.py
@@ -1,46 +1,50 @@
 import random
 
-MAYUSCULAS = "ABCDEFJHIJKLMNOPQRSTUVWKYZ"
-MINUSCULAS = "abcdefjhijlkmnoprqstuvwkyz"
-ESPECIALES = "$%&€@#+Ç"
-NUMEROS = "1234567890"
-
-password = ""
+MAYUSCULAS = "ABCDEFJHIJKLMNOPQRSTUVWKYZ" # Todas las letras mayusculas menos la ñ
+MINUSCULAS = "abcdefjhijlkmnoprqstuvwkyz" # Todas las letras minusculas menos la ñ
+ESPECIALES = "$%&€@#+Ç" # Caracteres especiales más importantes
+NUMEROS = "1234567890" # Números enteros
 
 # 1. Decidir los requisitos de la contraseña
-longitud = 0
+''' Validacion para que si o si el usuario introduzca un número entero'''
+longitud = 0 
 while longitud == 0: 
     try:
-        longitud = int(input("¿De que tamaño deseas la contraseña?: "))
+        longitud = int(input("¿De que tamaño deseas la contraseña?: ")) # Tamaño de la contraseña
     except ValueError: 
-        print("Tienes que introducir un número entero")
+        print("Tienes que introducir un número entero") # Si introducimos algo que no sea un entero, devuelve este mensaje pero no detiene la ejecucion
 
+''' Validacion para que solo se pueda introducir si o no'''
 validate_especial = False
 while validate_especial == False:
-    decidir_especial = input("Deseas que tu contraseña incluya caracteres especiales?: ")
+    decidir_especial = input("Deseas que tu contraseña incluya caracteres especiales? (si|no): ") # Decidimos si queremos especiales o no
     if decidir_especial.lower() == "si" or decidir_especial == "no": 
         validate_especial = True
     else: 
-        print("Tienes que escribir si o no")
+        print("Tienes que escribir si o no") # Si introducimos algo diferente, devuelve el mensaje pero no detiene la ejecucion
         validate_especial = False
 
+''' Validacion para que solo pueda introducir si o no'''
 validate_number = False
 while validate_number == False: 
-    decidir_numero = input("¿Deseas que tu contraseña incluya números?: ")
+    decidir_numero = input("¿Deseas que tu contraseña incluya números? (si|no): ") # Decidimos si queremos numeros o no
     if decidir_numero.lower() == "si" or decidir_numero == "no": 
         validate_number = True
     else: 
-        print("Tienes que escribir si o no")
+        print("Tienes que escribir si o no") # Si introducimos algo diferente, devuelve el mensaje pero no detiene la ejecucion
         validate_number = False
 
 
 # 2. Generar caracter aleatorio
 
 def caracter_aleatorio(especial, numero): 
+    ''' Una funcion que devuelve un caracter aleatorio, dependiendo de nuestras elecciones en el paso anterior'''
+    ''' Funciona con random, eligiendo numeros aleatorios asociados a cada decision y haciendo la eleccion sobre la longitud de la cadena'''
+    
     caracter = ""
-    if especial.lower() == "si":
-        if numero == "si":
-            aleatorio = random.randint(1,8)
+    if especial.lower() == "si": # Si elegimos que si queremos especiales y números
+        if numero == "si": 
+            aleatorio = random.randint(1,8) 
             if aleatorio == 1 or aleatorio == 8:
                 caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))] 
             elif aleatorio == 2 or aleatorio == 7:
@@ -49,7 +53,7 @@ def caracter_aleatorio(especial, numero):
                 caracter = ESPECIALES[random.randint(0, (len(ESPECIALES)-1))]
             else: 
                 caracter = NUMEROS[random.randint(0, len(NUMEROS) -1)]
-        else: 
+        else: # Si queremos especiales, pero no numeros
             aleatorio = random.randint(1,6)
             if aleatorio == 1 or aleatorio == 6:
                 caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))] 
@@ -57,7 +61,7 @@ def caracter_aleatorio(especial, numero):
                 caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
             else:
                 caracter = ESPECIALES[random.randint(0, (len(ESPECIALES)-1))]
-    if especial.lower() == "no": 
+    if especial.lower() == "no": # SI no queremos especiales, pero si numeros
         if numero == "si":
             aleatorio = random.randint(1,6)
             if aleatorio == 1 or aleatorio == 6:
@@ -66,7 +70,7 @@ def caracter_aleatorio(especial, numero):
                 caracter = MINUSCULAS[random.randint(0, (len(MINUSCULAS) -1))]
             else:
                 caracter = NUMEROS[random.randint(0, (len(NUMEROS)-1))]
-        if numero == "no":
+        if numero == "no": # Si no queremos especiales ni numeros
             aleatorio = random.randint(1,4)
             if aleatorio == 1 or aleatorio == 4: 
                 caracter = MAYUSCULAS[random.randint(0, (len(MAYUSCULAS)-1))]
@@ -75,11 +79,13 @@ def caracter_aleatorio(especial, numero):
                 
 
 
-    return caracter
+    return caracter # Devuelve el caracter aleatorio
 
 # 3. Generar contraseña
 
 def generar_contraseña(longitud): 
+    ''' Una función que genera una contraseña, en funcion de la longitud elegida y va añadiendo caracteres aleatorios a una cadena vacía'''
+
     password = ""
     while len(password) < longitud: 
         password += caracter_aleatorio(especial= decidir_especial, numero= decidir_numero)
---
+++ README.md
@@ -0,0 +1,104 @@
+# Generador de Contraseñas Aleatorias
+
+Este es un proyecto en Python que genera contraseñas seguras y aleatorias. El programa permite al usuario definir la longitud de la contraseña y elegir si desea incluir caracteres especiales y números.
+
+## Características
+
+- Define la longitud de la contraseña.
+- Opción de incluir o no caracteres especiales.
+- Opción de incluir o no números.
+- Generación completamente aleatoria.
+
+## Requisitos
+
+- Python 3.x
+
+## Instrucciones de Uso
+
+1. Clona este repositorio en tu máquina local:
+    ```bash
+    git clone https://github.com/jrramoncp/generador-contraseñas.git
+    ```
+
+2. Navega al directorio del proyecto:
+    ```bash
+    cd generador-contraseñas
+    ```
+
+3. Ejecuta el script:
+    ```bash
+    python generador_contraseñas.py
+    ```
+
+4. Sigue las instrucciones en pantalla:
+    - Introduce la longitud de la contraseña.
+    - Elige si quieres incluir caracteres especiales (si/no).
+    - Elige si quieres incluir números (si/no).
+
+5. El programa generará y te mostrará una contraseña aleatoria según tus elecciones.
+
+## Ejemplo
+
+
+¡Buena idea! Añadir un archivo README.md hará que tu proyecto sea más claro para otras personas y te ayudará a organizar la información sobre su uso y propósito.
+
+Aquí te dejo un ejemplo básico de un README.md para tu proyecto de Generador de Contraseñas:
+
+markdown
+Copiar código
+# Generador de Contraseñas Aleatorias
+
+Este es un proyecto en Python que genera contraseñas seguras y aleatorias. El programa permite al usuario definir la longitud de la contraseña y elegir si desea incluir caracteres especiales y números.
+
+## Características
+
+- Define la longitud de la contraseña.
+- Opción de incluir o no caracteres especiales.
+- Opción de incluir o no números.
+- Generación completamente aleatoria.
+
+## Requisitos
+
+- Python 3.x
+
+## Instrucciones de Uso
+
+1. Clona este repositorio en tu máquina local:
+    ```bash
+    git clone https://github.com/jrramoncp/generador-contraseñas.git
+    ```
+
+2. Navega al directorio del proyecto:
+    ```bash
+    cd generador-contraseñas
+    ```
+
+3. Ejecuta el script:
+    ```bash
+    python generador_contraseñas.py
+    ```
+
+4. Sigue las instrucciones en pantalla:
+    - Introduce la longitud de la contraseña.
+    - Elige si quieres incluir caracteres especiales (si/no).
+    - Elige si quieres incluir números (si/no).
+
+5. El programa generará y te mostrará una contraseña aleatoria según tus elecciones.
+
+## Ejemplo
+
+¿De qué tamaño deseas la contraseña?: 12 
+¿Deseas que tu contraseña incluya caracteres especiales? (si/no): si 
+¿Deseas que tu contraseña incluya números? (si/no): no 
+Aquí tienes tu contraseña: Ab%kKd&FkGjZ
+
+## Contribuciones
+
+Las contribuciones son bienvenidas. Si tienes ideas o mejoras, siéntete libre de abrir un **pull request** o crear un **issue**.
+
+## Autor
+
+jrramoncp
+
+Encuentrame en GitHub: [jrramoncp](https://github.com/jrramoncp)
+También puedes enviarme un mail: joseramoncaravaca@gmail.com

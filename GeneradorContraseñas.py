bc0346b592c1d2015b7505e1d96a6b1ae0efce2d
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2024-09-22 12:30:15 +0200

    Implemento funcion para generar contrase, y la imprimo

--- GeneradorContraseñas.py
+++ GeneradorContraseñas.py
@@ -9,11 +9,11 @@ password = ""
 
 # 1. Decidir los requisitos de la contraseña
 
-# longitud = int(input("¿De que tamaño deseas la contraseña?"))
-decidir_especial = input("¿Deseas que tu contraseña incluya caracteres especiales?")
-decidir_numero = input("¿Deseas que tu contraseña incluya números?")
+longitud = int(input("¿De que tamaño deseas la contraseña?: "))
+decidir_especial = input("¿Deseas que tu contraseña incluya caracteres especiales?: ")
+decidir_numero = input("¿Deseas que tu contraseña incluya números?: ")
 
-# 2. Generar la contraseña
+# 2. Generar caracter aleatorio
 
 def caracter_aleatorio(especial, numero): 
     caracter = ""
@@ -27,7 +27,7 @@ def caracter_aleatorio(especial, numero):
             elif aleatorio == 3 or aleatorio == 6: 
                 caracter = ESPECIALES[random.randint(0, (len(ESPECIALES)-1))]
             else: 
-                caracter = NUMEROS[random.randint(0, len(NUMEROS))]
+                caracter = NUMEROS[random.randint(0, len(NUMEROS) -1)]
         else: 
             aleatorio = random.randint(1,6)
             if aleatorio == 1 or aleatorio == 6:
@@ -56,13 +56,18 @@ def caracter_aleatorio(especial, numero):
 
     return caracter
 
-print(caracter_aleatorio(decidir_especial, decidir_numero))
+# 3. Generar contraseña
+
+def generar_contraseña(longitud): 
+    password = ""
+    while len(password) < longitud: 
+        password += caracter_aleatorio(especial= decidir_especial, numero= decidir_numero)
     
-# def generar_contraseña(longitud, caracter): 
-#     if caracter.lower() == "si": 
-#         contraseña = ""
-#         while len(contraseña) != longitud: 
-            
-#                 contraseña += 
+    return password
+    
+
 
 # 3. Mostrar contraseña generada
+
+print("Aqui tienes tu contraseña")
+print(generar_contraseña(longitud))
